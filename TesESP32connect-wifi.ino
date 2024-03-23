cc7033b433d7a56de1fa41e1716d2f96ffdb6600
Rifki Akmal Alauddin <68954900+RifkiAkmal@users.noreply.github.com>
2024-03-23 15:00:18 +0700

    Update TesESP32connect-wifi.ino

--- TesESP32connect-wifi.ino
+++ TesESP32connect-wifi.ino
@@ -13,11 +13,9 @@ int ledPin = 19;
 void setup()
 {
     Serial.begin(115200);
-    pinMode(ledPin, OUTPUT);      // set the LED pin mode
+    pinMode(ledPin, OUTPUT);
 
     delay(10);
-
-    // We start by connecting to a WiFi network
 
     Serial.println();
     Serial.println();
@@ -43,54 +41,44 @@ void setup()
 }
 
 void loop(){
- WiFiClient client = server.available();   // listen for incoming clients
+ WiFiClient client = server.available();   
 
-  if (client) {                             // if you get a client,
-    Serial.println("New Client.");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
+  if (client) { 
+    Serial.println("New Client."); 
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n') {
 
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println();
 
-            // the content of the HTTP response follows the header:
             client.print("Rifki Akmal Alauddin<br>");
             client.print("NPM 187002015<br>");
             client.print(" <br>");
             client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 19 on.<br>");
             client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 19 off.<br>");
 
-            // The HTTP response ends with another blank line:
             client.println();
-            // break out of the while loop:
             break;
-          } else {    // if you got a newline, then clear currentLine:
+          } else {
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') {
+          currentLine += c;
         }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
         if (currentLine.endsWith("GET /H")) {
-          digitalWrite(ledPin, HIGH);               // GET /H turns the LED on
+          digitalWrite(ledPin, HIGH);
         }
         if (currentLine.endsWith("GET /L")) {
-          digitalWrite(ledPin, LOW);                // GET /L turns the LED off
+          digitalWrite(ledPin, LOW);
         }
       }
     }
-    // close the connection:
     client.stop();
     Serial.println("Client Disconnected.");
   }
